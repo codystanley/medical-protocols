3c2d3ad9d906579bf8388a1bc79bbc46d705f924
codystanley <cody.stanley@triagelogic.com>
2024-07-07 16:47:36 -0400

    updated config to normal db credentials

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$servername = getenv('CLOUD_SQL_HOST'); // Or $_ENV['CLOUD_SQL_HOST'] or $_SERVER['CLOUD_SQL_HOST']
-$username = getenv('CLOUD_SQL_USER');
-$password = getenv('CLOUD_SQL_PASSWORD');
-$dbname = getenv('CLOUD_SQL_DATABASE');
+$servername = '34.23.248.39'; // Or $_ENV['CLOUD_SQL_HOST'] or $_SERVER['CLOUD_SQL_HOST']
+$username = 'pm-user';
+$password = 'Password123!';
+$dbname = 'AAH_2024';
